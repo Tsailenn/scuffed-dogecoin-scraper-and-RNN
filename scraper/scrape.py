e620076e55e0b04c8dd7b4b2240e1819a0204327
Tsailenn <70084579+Tsailenn@users.noreply.github.com>
2021-05-12 19:03:15 +0700

    Add files via upload

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -31,6 +31,10 @@ class Spyder:
             except:
                 break
         self.f.close()
+        print('finished')
+        # test = WebDriverWait(self.driver, timeout=20).until(lambda d: d.find_element_by_xpath('//*[@id="__next"]/div/div[1]/div[2]/div/div[3]/div/div/div[2]/table/tbody'))
+        # test = test.find_elements(By.XPATH, "./child::*")
+        # print(len(test))
         #//*[@id="__next"]/div/div[1]/div[2]/div/div[3]/div/div/div[2]/table/tbody/tr[1]
         #//*[@id="__next"]/div/div[1]/div[2]/div/div[3]/div/div/div[2]/table/tbody/tr[2]
         #//*[@id="__next"]/div/div[1]/div[2]/div/div[3]/div/div/div[2]/table/tbody/tr[150]
@@ -41,15 +45,28 @@ class Spyder:
 
     def ClickButton(self):
         #//*[@id="__next"]/div/div[1]/div[2]/div/div[3]/div/div/p[1]/button
+        preload_length = self.CheckLength()
+        time.sleep(6)
         btn = WebDriverWait(self.driver, timeout=20).until(lambda d: d.find_element_by_xpath('//*[@id="__next"]/div/div[1]/div[2]/div/div[3]/div/div/p[1]/button'))
-        time.sleep(3)
+        time.sleep(6)
         btn.send_keys(Keys.END)
-        time.sleep(3)
+        time.sleep(6)
         btn.click()
+        time.sleep(6)
+        postload_length = self.CheckLength()
+        time.sleep(6)
+        if(preload_length == postload_length):
+            print('stopped')
+            self.run = False
         #print(btn)
 
         #l = WebDriverWait(self.driver, timeout=20).until(lambda d: d.find_element_by_xpath('//*[@id="__next"]/div/div[1]/div[2]/div/div[3]/div/div/div[2]/table/tbody'))
         #print(l.text)
+    
+    def CheckLength(self):
+        l = WebDriverWait(self.driver, timeout=20).until(lambda d: d.find_element_by_xpath('//*[@id="__next"]/div/div[1]/div[2]/div/div[3]/div/div/div[2]/table/tbody'))
+        l = l.find_elements(By.XPATH, "./child::*")
+        return len(l)
         
 if __name__ == "__main__":
     spyder = Spyder()
